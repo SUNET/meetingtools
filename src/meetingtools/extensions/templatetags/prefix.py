739c1c3780cdde3bc371702a6b946ab2d0787bcc
Leif Johansson <leifj@sunet.se>
2011-06-01 22:29:36 +0200

    absolute url for validation links

--- src/meetingtools/extensions/templatetags/prefix.py
+++ src/meetingtools/extensions/templatetags/prefix.py
@@ -5,8 +5,13 @@ Created on Feb 9, 2011
 '''
 
 from django import template
-from meetingtools.settings import PREFIX_URL
+from meetingtools.settings import PREFIX_URL, BASE_URL
 register = template.Library()
+
+@register.simple_tag
+def baseurl():
+    return BASE_URL
+
 
 @register.simple_tag
 def prefix():
