bd015bbb282eb4093b888ac929b98d2ef620cfec
Leif Johansson <leifj@sunet.se>
2011-08-03 15:22:50 +0200

    avoid unknown user count

--- src/meetingtools/apps/room/views.py
+++ src/meetingtools/apps/room/views.py
@@ -255,7 +255,7 @@ def _import_room(request,acc,r):
     for key in ('sco_id','name','source_sco_id','urlpath','description','user_count','host_count'):
         if r.has_key(key) and hasattr(room,key):
             rv = getattr(room,key)
-            if rv != r[key] and r[key]:
+            if rv != r[key] and r[key] != None and r[key] != '':
                 setattr(room,key,r[key])
                 modified = True
     
