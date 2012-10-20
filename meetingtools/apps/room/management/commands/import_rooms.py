1907bfa78a3f2364e0856507c78e5eb09a1692f1
Leif Johansson <leifj@sunet.se>
2012-10-21 00:03:48 +0200

    option for since

--- meetingtools/apps/room/management/commands/import_rooms.py
+++ meetingtools/apps/room/management/commands/import_rooms.py
@@ -1,3 +1,4 @@
+from optparse import make_option
 from django.core.management import BaseCommand
 from meetingtools.apps.cluster.models import ACCluster
 from meetingtools.apps.room.tasks import import_acc
@@ -6,6 +7,14 @@ __author__ = 'leifj'
 
 class Command(BaseCommand):
 
+    option_list = BaseCommand.option_list + (
+        make_option('--since',
+            type='int',
+            dest='since',
+            default=0,
+            help='Import all rooms modified <since> seconds ago'),
+        )
+
     def handle(self, *args, **options):
         for acc in ACCluster.objects.all():
-            import_acc(acc,since=0)
+            import_acc(acc,since=options['since'])
